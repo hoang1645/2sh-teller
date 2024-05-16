00b69c25261eeb594d22e269d306d5f30982ab3d
hoang1645 <hoang.208.1974@gmail.com>
2024-05-16 10:33:52 +0700

    completing outline for finetuning and inference

--- backend/model/model.py
+++ backend/model/model.py
@@ -1,26 +1,44 @@
-from typing import Callable, Literal, Tuple, Union
+from typing import Callable, Literal, Tuple, List, Any
 
 import torch
+from torch.utils.data import Dataset
+from peft import LoraConfig, prepare_model_for_kbit_training, get_peft_model
+from pyreft import get_reft_model, ReftConfig, LoreftIntervention, ReftTrainerForCausalLM
 from bitsandbytes.optim import PagedAdamW8bit
-from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    BitsAndBytesConfig,
+    Trainer as HFTrainer,
+    TrainingArguments,
+    DataCollatorForLanguageModeling,
+    GenerationConfig,
+)
 
 
+# TODO: add comprehensive docstrings for important methods
 class Llama3Model:
     def __init__(
         self,
         model: Literal['8B', '70B', '8B-Instruct', '70B-Instruct'] = "8B-Instruct",
+        custom_checkpoint_path: str = None,
         load_in_n_bits: Literal[4, 8, 16, 32] = 4,
-        efficient_finetuning_method: Literal["lora", "qlora", "reft", None] = None,
-        lr: float = 3e-4,
-        betas: Tuple[float, float] = (0.9, 0.98),
-        weight_decay: float = 1e-3,
-        eps: float = 1e-8,
+        efficient_finetuning_method: Literal["lora", "reft", None] = None,
+        lora_apply_layers: List[str] = ["self_attn", "mlp", "embed_tokens"],
+        lora_dropout: float = 0.1,
+        reft_component: int | str = None,
+        reft_low_rank_dim: int = None,
     ):
         """
         Wrapper for Llama 3 model, including training and inference.
         """
 
+        # save important args to use in other methods
         self.trainable = True
+        self.load_in_nbits = load_in_n_bits
+        self.efficient_finetuning_method = efficient_finetuning_method
+
+        # check args
         if load_in_n_bits <= 8 and efficient_finetuning_method is None:
             self.trainable = False
         if load_in_n_bits <= 8 and efficient_finetuning_method == "reft":
@@ -31,6 +49,7 @@ class Llama3Model:
 
         # quantization configurations
         quantization_config = {"load_in_4bit": False, "load_in_8bit": False}
+        # 4bit (for qlora)
         if load_in_n_bits == 4:
             quantization_config["load_in_4bit"] = True
             quantization_config["quantization_config"] = BitsAndBytesConfig(
@@ -40,40 +59,135 @@ class Llama3Model:
                 **quantization_config,
             )
             quantization_config["torch_dtype"] = torch.bfloat16
+        # 8bit (llm-int8)
         if load_in_n_bits == 8:
             quantization_config["load_in_8bit"] = True
             quantization_config["quantization_config"] = BitsAndBytesConfig(
-                llm_int8_skip_modules="lm_head", **quantization_config
+                llm_int8_skip_modules="lm_head",
+                llm_int8_has_fp16_weight=True,
+                **quantization_config,
             )
             quantization_config["torch_dtype"] = torch.bfloat16
-        else:
+        # 16bit (bf16)
+        if load_in_n_bits == 16:
+            quantization_config['torch_dtype'] = torch.bfloat16
+        # 32bit (fp32)
+        if load_in_n_bits == 32:
             quantization_config['torch_dtype'] = torch.float32
-
+        # model initialization
         self.model_id = f"meta-llama/Meta-Llama-3-{model}"
         self.tokenizer = AutoTokenizer.from_pretrained(self.model_id)
-        self.model = AutoModelForCausalLM(self.model_id, **quantization_config)
-
-        self.optimizer = self.__configure_optimizer(
-            lr=lr, betas=betas, weight_decay=weight_decay, eps=eps, **quantization_config
+        self.model = AutoModelForCausalLM(
+            custom_checkpoint_path
+            or self.model_id,  # load checkpoint if exists, if not load the baseline
+            **quantization_config,
         )
+        # optimizer
+        self.optimizer = self.__optimizer(**quantization_config)
+        # prepare model for quantization training
+        if load_in_n_bits <= 8:
+            self.model = prepare_model_for_kbit_training(self.model)
+
+        # lora setup
+        if efficient_finetuning_method == 'lora':
+            config = LoraConfig(
+                target_modules=lora_apply_layers, task_type="CAUSAL_LM", lora_dropout=lora_dropout
+            )
+
+            self.model = get_peft_model(self.model, config)
+
+        # reft setup
+        if efficient_finetuning_method == 'reft':
+            config = ReftConfig(
+                representations={
+                    "component": reft_component,
+                    "low_rank_dimension": reft_low_rank_dim,
+                    "intervention": LoreftIntervention(
+                        embed_dim=self.model.config.hidden_size,
+                        low_rank_dimension=reft_low_rank_dim,
+                    ),
+                },
+                low_rank_dimension=reft_low_rank_dim,
+            )
+            self.model = get_reft_model(self.model, config)
 
     def __optimizer(
         self, load_in_4bit: bool, load_in_8bit: bool
     ) -> Callable[..., torch.optim.Optimizer]:
         if load_in_4bit or load_in_8bit:
-            return PagedAdamW8bit
-        return torch.optim.AdamW
+            return "paged_adamw_8bit"
+        return "adamw"
 
-    def __configure_optimizer(
+    def save_checkpoint(self, save_path: str):
+        self.model.save_pretrained(save_path)
+
+    def finetune(
         self,
-        load_in_4bit: bool,
-        load_in_8bit: bool,
-        lr: float,
-        betas: Tuple[float, float],
-        weight_decay: float,
-        eps: float,
-        **unused_kwargs,
+        dataset: Dataset,
+        batch_size: int,
+        gradient_accumulation_steps: int = 1,
+        data_collator: Callable[..., Any] = None,
+        lr: float = 3e-4,
+        betas: Tuple[float, float] = (0.9, 0.98),
+        weight_decay: float = 1e-3,
+        eps: float = 1e-8,
+        epochs: int = 3,
     ):
-        return self.__optimizer(load_in_4bit, load_in_8bit)(
-            lr=lr, betas=betas, weight_decay=weight_decay, eps=eps
+        if not self.trainable:
+            ValueError("currently not supporting finetuning with this model's configurations")
+        training_args = TrainingArguments(
+            "./out",
+            per_device_train_batch_size=batch_size,
+            optim=self.optimizer,
+            bf16=(self.load_in_nbits <= 16),
+            learning_rate=lr,
+            adam_beta1=betas[0],
+            adam_beta2=betas[1],
+            adam_epsilon=eps,
+            weight_decay=weight_decay,
+            num_train_epochs=epochs,
+            gradient_accumulation_steps=gradient_accumulation_steps,
         )
+
+        if data_collator is None:
+            data_collator = DataCollatorForLanguageModeling(self.tokenizer, mlm=False)
+        if self.efficient_finetuning_method != "reft":
+            trainer = HFTrainer(
+                self.model,
+                training_args,
+                data_collator=data_collator,
+                train_dataset=dataset,
+                tokenizer=self.tokenizer,
+            )
+        else:
+            trainer = ReftTrainerForCausalLM(
+                self.model,
+                training_args,
+                data_collator=data_collator,
+                train_dataset=dataset,
+                tokenizer=self.tokenizer,
+            )
+
+        trainer.train()
+
+    def generate(
+        self,
+        sentences: List[str],
+        beam_size: int = 1,
+        n_return_sequences: int = 1,
+        top_k: int = 50,
+        top_p: float = 1,
+        temperature: float = 1,
+    ):
+        lengths = [len(sentence) for sentence in sentences]
+        input_ids = self.tokenizer(sentences, padding=True, return_tensors='pt').input_ids
+        gen_config = GenerationConfig(
+            beam_size=beam_size,
+            n_return_sequences=n_return_sequences,
+            top_k=top_k,
+            top_p=top_p,
+            temperature=temperature,
+        )
+        out_tokens = self.model.generate(input_ids, gen_config)
+        out_sentences = self.tokenizer.batch_decode(out_tokens, skip_special_tokens=True)
+        return [out_sentences[i][length:] for i, length in enumerate(lengths)]
