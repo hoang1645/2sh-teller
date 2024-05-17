7a5f0f2bef49f6a5c676f40031f505de5cf57686
hoang1645 <hoang.208.1974@gmail.com>
2024-05-17 10:16:22 +0700

    fix the model dict key

--- backend/model/train.py
+++ backend/model/train.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     with open("training-configs.yaml", encoding="utf8") as conf:
         args = yaml.load(conf, yaml.SafeLoader)
 
-    model = Llama3Model(args["model"], args["from_ckpt"], **args['training'])
+    model = Llama3Model(args["model"], args["custom_checkpoint_path"], **args['training'])
     dataset = CustomDataset("dataset/dset.json")
     model.finetune(dataset, epochs=args['epochs'], **args['optimization'])
     model.save_checkpoint("./checkpoints")
