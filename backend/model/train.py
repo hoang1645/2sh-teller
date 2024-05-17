aebd6509112f5671dae035c9390109098ccb7387
hoang1645 <hoang.208.1974@gmail.com>
2024-05-17 10:15:00 +0700

    fix the arg calls

--- backend/model/train.py
+++ backend/model/train.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     with open("training-configs.yaml", encoding="utf8") as conf:
         args = yaml.load(conf, yaml.SafeLoader)
 
-    model = Llama3Model(args.model_size, args.from_ckpt, **args['training'])
+    model = Llama3Model(args["model_size"], args["from_ckpt"], **args['training'])
     dataset = CustomDataset("dataset/dset.json")
     model.finetune(dataset, epochs=args['epochs'], **args['optimization'])
     model.save_checkpoint("./checkpoints")
